326f7f4a3ff3f0020cd7a7ef673ac89329b3dcdf
Anders Klund-Hansen <anders.klund.hansen@gmail.com>
2022-08-10 15:19:38 +0200

    Removed unused css

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -2,41 +2,32 @@
 </script>
 
 <div class="tile">
-    <div class="logo-container">
-        <img src="img/itera.png" alt="Logo" />
-        <h1>Fredrikstad</h1>
-    </div>
+  <div class="logo-container">
+    <img src="img/itera.png" alt="Logo" />
+    <h1>Fredrikstad</h1>
+  </div>
 </div>
 
 <style>
-    .logo-container {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+  .logo-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-    .logo-container img {
-        width: 48px;
-        border-radius: 0.25rem;
-    }
+  .logo-container img {
+    width: 48px;
+    border-radius: 0.25rem;
+  }
 
-    .logo-container h1 {
-        font-size: 2rem;
-        font-family: "Feeling Passionate";
-        margin: 0;
-        margin-top: 1.5rem;
-        line-height: 1;
-    }
-
-    .logo-container h2 {
-        font-size: 0.8rem;
-        font-family: "IBM Plex Sans";
-        margin: 0;
-        margin-top: 3rem;
-        font-weight: 300;
-        text-transform: uppercase;
-    }
+  .logo-container h1 {
+    font-size: 2rem;
+    font-family: "Feeling Passionate";
+    margin: 0;
+    margin-top: 1.5rem;
+    line-height: 1;
+  }
 </style>
