7f402de8d4387b40c475c060432412bbfefa14dd
thrandre <thomas.r.andresen@hotmail.no>
2022-08-19 14:07:07 +0200

    More hack and slash

--- svelte.config.js
+++ svelte.config.js
@@ -4,17 +4,6 @@ export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess({
-    postcss: true,
-    babel: {
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            useBuiltIns: "entry",
-            corejs: "3.22",
-          },
-        ],
-      ],
-    },
+    postcss: true
   }),
 };
